118dc1b937f25fe09c5f91d1711cf3cf24d26681
Michael Raynes <ouo655@my.utsa.edu>
2019-10-05 20:06:26 -0500

    Adding Task 6 Requirements for the the Book information restrictions

--- src/main/java/javafx/controller/BookCreateController.java
+++ src/main/java/javafx/controller/BookCreateController.java
@@ -15,6 +15,7 @@ import javafx.model.BookGateway;
 import javafx.model.BookTableGatewayMySQL;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 
 import java.util.List;
 
@@ -33,6 +34,9 @@ public class BookCreateController {
 
     @FXML
     private TextArea labelSummary;
+
+    @FXML
+    private Text alertISBN, alertYear, alertTitle, alertSummary;
 
     @FXML
     private Button buttonCreate;
@@ -64,11 +68,39 @@ public class BookCreateController {
 	@FXML public void onCreate() {
     	logger.info("@BookCreateController onSearch()");
     	
+    	String title = labelTitle.getText();
+    	String isbn = labelISBN.getText();
+    	String yearText = labelYearPublished.getText();
+    	String summary = labelSummary.getText();
+    	
+
+//    	a. title must be between 1 and 255 chars
+    	if(title.length() < 1 || title.length() > 255) {
+    		alertTitle.setText("title must be between 1 and 255 chars");
+    	}
+
+//    	b. summary must be < 65536 characters. can be blank
+    	if(summary.length() > 65536) {
+    		alertSummary.setText("summary must be < 65536 characters. can be blank");
+    	}
+    	
+    	int yearPublished = Integer.parseInt(labelYearPublished.getText());
+//    	c. year_published must be between 1455 and the current year (inclusive)
+    	if(yearPublished < 1455  || yearPublished > 2019) {
+    		alertISBN.setText("1455 up to current year");
+    	}
+    	
+
+//    	int isbn = Integer.parseInt(labelISBN.getText());
+//    	d. isbn cannot be > 13 characters. can be blank Implement these business rules as validation methods 
+    	if(isbn.length() > 13) {
+    		alertYear.setText("Cannot be > 13 characters.");
+    	}
+    	
     	try {	
 
     	
-    	MainController.getBookGateway().createBook(labelTitle.getText(), Integer.parseInt(labelISBN.getText())
-    			,Integer.parseInt(labelYearPublished.getText()),labelSummary.getText());
+//    	MainController.getBookGateway().createBook(title, isbn, yearPublished, summary);
     	
     	}catch(Exception e){
     		System.out.println(labelTitle.getText()+Integer.parseInt(labelISBN.getText().trim())
